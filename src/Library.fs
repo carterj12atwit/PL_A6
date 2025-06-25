db40bea683fc0c75658655c7a052aecd5912c69e
Jake Carter <carterj12@wit.edu>
2025-06-25 16:08:02 -0400

    completed and documented the first four problems

--- src/Library.fs
+++ src/Library.fs
@@ -1,29 +1,42 @@
 ﻿namespace wit.comp3350
 
 module a6 =
-    /// TODO: Complete and document
+    /// <summary> sorts the contents of a string list in a case insensitive manner </summary>
+    /// <params> a string list </params>
+    /// <returns> the sorted string list </returns>
     let sortCaseInsensitive (l: string list) =
-        ["A"]
+        l |> List.sortBy (fun x -> x.ToUpper())
 
-    /// TODO: Complete and document
+    /// <summary> sorts the names within a tuple pair, and then sorts the tuples themselves </summary>
+    /// <params> a list of string pairs </params>
+    /// <returns> an internally and externally sorted string pair list </returns>
     let sortPairs (l: (string*string) list) =
-        [("A", "B")]        
+        l 
+        |> List.map (fun (x, y) -> if x.ToUpper() <= y.ToUpper() then x, y else y, x)
+        |> List.sortBy (fun (x, y) -> x.ToUpper(), y.ToUpper())  
 
     /// List of valid characters for Flipper
     let validChars = ['I'; 'O'; 'S'; 'H'; 'Z'; 'X'; 'N']
     
-    /// TODO: Complete and document
+    /// <summary> determines if the word could theoretically be flipped and still be legible </summary>
+    /// <params> a list of valid characters and  a list of chars in the word </params>
+    /// <returns> a boolean value indicating whether the word is valid </returns>
     let rotatingLetters l =
-        false
+        l |> List.forall (fun x -> List.contains x validChars)
     
-    /// TODO: Complete and document
+    /// <summary> function that can flip a grid of numbers either horizontally and vertically </summary>
+    /// <params> a grid and an either horizontal or vertical command </params>
+    /// <returns> either a horizontally or vertically flipped version of the given grid </returns>
     let flip grid command =
-        let a, b, c, d = grid
-        (a, b, c, d)
+        match grid, command with
+        | (a, b, c, d), 'h' -> c, d, a, b
+        | (a, b, c, d), 'v' -> b, a, d, c 
 
-    /// TODO: Complete and document
+    /// <summary> function that calls the horizontal and vertical flips </summary>
+    /// <params> a list of commands </params>
+    /// <returns> the final list after all horizontal/vertical flips have been executed </returns>
     let flipAll commands =
-        (1, 2, 3, 4)
+        List.fold flip (1, 2, 3, 4) commands
 
     /// <summary>This type represents a binary tree. A binary tree can either be
     ///  - An empty node
